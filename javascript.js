4501422d18f9ef628362d27804750f4efd507be6
Jesper Jensen sverd <jesperjensensverd@Jespers-MacBook-Pro.local>
2018-10-05 10:30:32 +0200

    loop

--- javascript.js
+++ javascript.js
@@ -2,15 +2,28 @@ var bird;
 
   var positionX = 0;
   var positionY = 0;
+  
 
 function startanimation() {
    bird = document.getElementById("Flyingbird");   
     setInterval(update, 20);
 }
 
-function update() {
-   bird.style.left = positionX + "%";
-   positionX += 1;
- if (bird )  
+
+
+ function update() {
+  positionX += 1;
+  if (positionX >100){
+     return positionX = -20 
+  } 
+
+
+
+  
+
+  
+  
+  bird.style.left = positionX + "%";
+  
 }
  
